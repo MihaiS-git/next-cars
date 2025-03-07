aeb93c7486def197e2aa57a6930bef25c66ada3e
MihaiS-git <ingsuciumihai@gmail.com>
2025-03-07 18:19:06 +0200

    fix dev google auth

--- auth.ts
+++ auth.ts
@@ -26,6 +26,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          redirect_uri: process.env.REDIRECT_URI,
+        },
+      },
     }),
   ],
 });
