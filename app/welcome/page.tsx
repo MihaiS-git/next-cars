9fbf7eca37417c41e7cf2a5ab86744472c6e03b8
MihaiS-git <ingsuciumihai@gmail.com>
2025-03-17 17:48:17 +0200

    add spinner on welcome page

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import AutoCarousel from "@/components/ui/welcome/AutoCarousel";
 import { getCarsImagesForWelcomeCarousel } from "@/lib/db/cars";
@@ -9,7 +9,7 @@ export default function WelcomePage() {
     const [error, setError] = useState("");
     const [carImages, setCarImages] = useState<string[]>([]);
 
-    useEffect(() => { 
+    useEffect(() => {
         async function getWelcomeCarouselImages() {
             const fetchedCarImages = await getCarsImagesForWelcomeCarousel();
             if (!fetchedCarImages) {
@@ -32,14 +32,19 @@ export default function WelcomePage() {
             </div>
         );
     }
-    
+
     return (
         <>
             {error && <h1>{error}</h1>}
-            {!error && carImages.length === 0 && <h1>No car images found</h1>}
-            {!error && carImages.length > 0 &&   
+            {!error && carImages.length === 0 && (
+                <div className="flex flex-col items-center justify-center">
+                    <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-red-600 m-8 mx-auto mt-48 mb-8"></div>
+                    <div className="text-zinc-400 mb-48">Loading images...</div>
+                </div>
+            )}
+            {!error && carImages.length > 0 && (
                 <AutoCarousel carImages={carImages} />
-            }
+            )}
         </>
     );
 }
